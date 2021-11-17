a4846f3e03b747fd3fc42c09f15f252e4564fd26
Milan Sterkens <71765443+MilanSterkens@users.noreply.github.com>
2021-11-17 19:09:02 +0100

    update slagboom_nummerplaat.py functie scanNummerplaat() aangepast met return type van string

--- Slagboom/programma's/slagboom_nummerplaat.py
+++ Slagboom/programma's/slagboom_nummerplaat.py
@@ -59,39 +59,25 @@ def meetAfstand(inOfUitgang):
     return round(((endtime - starttime)*17000),0)
 
 def scanNummerplaat():
-    ret, frame = cam.read()
-    
-    image = cam.read()
+    ret, frame, image = cam.read()
+    img_counter=0
     key = cv2.waitKey(1) & 0xFF
-    #rawCapture.truncate(0)
-    
-    if key == ord("s"):
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        img=Image.open(r'/home/pi/opencv_frame_{}.png'.format(img_counter))
-        text = pytesseract.image_to_string(img)
-        print(text)
-        #cv2.imshow("Frame", image)
-        cv2.waitKey(0)
-        img_counter += 1
-        break
     
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
+    img_name = "opencv_frame_{}.png".format(img_counter)
+    cv2.imwrite(img_name, frame)
+    img=Image.open(r'/home/pi/opencv_frame_{}.png'.format(img_counter))
+    text = pytesseract.image_to_string(img)
+    print(text)
+    cv2.waitKey(0)
+    img_counter += 1
+    return text
+    
+   
+ 
 
 try:
     while True:
