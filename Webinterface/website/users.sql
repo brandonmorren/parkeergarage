aa010a15818dc902be8c45b14ff8dcf0555171dc
Frederik <57659570+Defreddy@users.noreply.github.com>
2021-11-26 14:47:04 +0100

    Removed and created new files

--- Webinterface/website/users.sql
+++ Webinterface/website/users.sql
@@ -1,42 +1,22 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Apr 28, 2017 at 02:20 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `demo`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `nrplaat` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `license` varchar(50) NULL,
+  `username` varchar(50) NULL,
+  `password` varchar(50) NULL,
+  `nummerplaat` varchar(50) NOT NULL,
+  `voornaam` varchar(50) NULL,
+  `achternaam` varchar(50) NULL,
   `parkingspot` INT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `automodel` INT NULL,
+  `aankomst` datetime DEFAULT CURRENT_TIMESTAMP,
+  `vertrek` datetime DEFAULT CURRENT_TIMESTAMP,
+  `tebetalen` INT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `nummerplaat` (`nummerplaat`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
